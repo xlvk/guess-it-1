1f2bf8596621b325e90051d2f0ba2283db53558a
fatabbas <fatima77614@gmail.com>
2023-09-05 20:06:00 +0300

    ?

--- main.go
+++ main.go
@@ -1,63 +1,39 @@
 package main
 
 import (
-	
 	"bufio"
 	"fmt"
-	"log"
-	"os"
+	// "log"
 	"math"
-	"strconv"
+	"os"
 	"sort"
+	"strconv"
 )
 
 func main() {
-
 	var arr []int
-var ForY []int
 	sum := 0.0
 	sd := 0.0
 	avr := 0.0
-	readFile, err := os.Open("data.txt")
-	defer readFile.Close()
-
-	if err != nil {
-		log.Fatalf("failed to open file: %s", err)
-	}
-
-	fileScanner := bufio.NewScanner(readFile)
-	fileScanner.Split(bufio.ScanLines)
-
+	fileScanner := bufio.NewScanner(os.Stdin)
 	for fileScanner.Scan() {
 		ha, e := strconv.Atoi(fileScanner.Text())
-    	if e != nil {
-        	fmt.Printf("%T \n %v", ha, ha)
+		if e != nil {
+			fmt.Printf("%T \n %v", ha, ha)
 		}
 		arr = append(arr, ha)
-		ForY = append(ForY, ha)
 		sum = sum + float64(ha)
-	}
-	
-	sort.Ints(arr)
-	x, e := strconv.Atoi(os.Args[1])
-    	if e != nil {
-        	fmt.Printf("%T \n %v", x, x)
+		sort.Ints(arr)
+		avr = math.Round(sum / float64(len(arr)))
+		for j := 0; j < len(arr); j++ {
+			sd += math.Pow((float64(arr[j]) - avr), 2)
 		}
-		if x > len(arr)-1 {
-			fmt.Println("Error: NO Y VALUE GIVEN ON THE", x , "VALUE.")
-			return
-		}
-	c := ForY[0]
-	m := float64(ForY[x+1]- ForY[0])/float64(x+1)
-	y := (m*float64(x+1)) + float64(c)
-	avr = math.Round(sum/float64(len(arr)))
-	for j := 0; j < len(arr); j++ {
-		sd += math.Pow((float64(arr[j])- avr), 2)
+		sd = math.Round(math.Sqrt(sd / float64(len(arr))))
+		y := avr - sd
+		low := int(y - 3)
+		up := int(y + 2)
+
+		fmt.Printf("%d %d\n" ,low, up)
 	}
-	sd = math.Round(math.Sqrt(sd / float64(len(arr))))
-	low := (y-(sd/8))
-	up := (y+(sd/8))
-	fmt.Println(low, "~" , up)
-	fmt.Println(ForY[x+1])
 
 }
